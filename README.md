2d80828da493f811f15a05f31546c6f0dcb05959
Chris Graham <Chrisgraham908@gmail.com>
2024-07-23 23:05:34 +1200

    Add local Jaeger tracing feature (#2)
    * Add tracing cfg
    
    * Add jaeger tracing
    
    * Add debug tracing layer (#5)

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Parkinson's Pulse Service
 The Backend service for parkinson's pulse
+
+# Tracing
+## Jaeger
+This service allows for local use of jaeger for tracing. 
+![jaeger_log_example.png](docs/jaeger_log_example.png)
+To enable this feature please follow the following steps.
+
+- Run Jaeger `docker run -d -p6831:6831/udp -p6832:6832/udp -p16686:16686 -p14268:14268 jaegertracing/all-in-one:latest`
+- Run with jaeger tracing feature enabled `cargo run --features jaeger_tracing`
+- Open Jaeger `http://localhost:16686/`
+
+If you are using JetBrains you can create a profile to auto run this feature:
+![JetBrainsJaegerFeat.png](docs/JetBrainsJaegerFeat.png)
--- src/tracing.rs
+++ src/tracing.rs
@@ -1,16 +1,47 @@
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use opentelemetry::global;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, Layer};
 
 /// Initializes tracing and logging for service
+///
+/// # Features
+/// local_log: local fmt logging, enabled by default \
+/// jaeger_tracing: This feature flag enabled jaeger tracing
 ///
 /// # Panics
 /// This function panics upon failing to create and or init tracing.
 ///
 pub fn init_tracing() {
-    // TODO!: Finish OpenTelemetry tracing so a collector can get it
-    let stdout_layer = tracing_subscriber::fmt::Layer::default();
+    let mut layers = Vec::new();
+
+    if cfg!(feature = "jaeger_tracing") {
+        global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
+
+        let jaeger_tracer = opentelemetry_jaeger::new_pipeline()
+            .with_service_name(env!("CARGO_PKG_NAME"))
+            .install_simple()
+            .expect("Failed to generate Jaeger tracing pipeline");
+
+        let jaeger_layer = tracing_opentelemetry::layer()
+            .with_tracer(jaeger_tracer)
+            .boxed();
+
+        layers.push(jaeger_layer);
+    }
+
+    if cfg!(feature = "local_log") {
+        let stdout_layer = tracing_subscriber::fmt::Layer::default().boxed();
+
+        layers.push(stdout_layer);
+    }
 
     tracing_subscriber::registry()
-        .with(stdout_layer)
+        .with(layers)
         .try_init()
         .expect("Could not init tracing registry");
+
+    #[cfg(feature = "jaeger_tracing")]
+    tracing::info!("Jaeger tracing enabled");
+
+    #[cfg(feature = "local_log")]
+    tracing::info!("Local logging fmt enabled");
 }
