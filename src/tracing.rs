ae61532365ed1d14ba993fc156e669568e21bde7
Chris Graham <Chrisgraham908@gmail.com>
2024-07-23 14:29:40 +1200

    Add tracing cfg

--- src/tracing.rs
+++ src/tracing.rs
@@ -1,3 +1,4 @@
+use opentelemetry::global;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 /// Initializes tracing and logging for service
@@ -6,11 +7,31 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 /// This function panics upon failing to create and or init tracing.
 ///
 pub fn init_tracing() {
-    // TODO!: Finish OpenTelemetry tracing so a collector can get it
-    let stdout_layer = tracing_subscriber::fmt::Layer::default();
+    if cfg!(enable_tracing) {
+        global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
 
-    tracing_subscriber::registry()
-        .with(stdout_layer)
-        .try_init()
-        .expect("Could not init tracing registry");
+        let tracer = opentelemetry_jaeger::new_pipeline()
+            .with_service_name("mini-redis")
+            .install_simple()
+            .expect("Failed to generate Jaeger tracing pipeline");
+
+        let jaeger_layer = tracing_opentelemetry::layer().with_tracer(tracer);
+        let stdout_layer = tracing_subscriber::fmt::Layer::default();
+
+        tracing_subscriber::registry()
+            .with(jaeger_layer)
+            .with(stdout_layer)
+            .try_init()
+            .expect("Could not init tracing registry");
+        
+        tracing::info!("Jaeger tracing enabled");
+    }
+    else {
+        let stdout_layer = tracing_subscriber::fmt::Layer::default();
+
+        tracing_subscriber::registry()
+            .with(stdout_layer)
+            .try_init()
+            .expect("Could not init tracing registry");
+    }
 }
