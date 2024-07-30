250e610fd00a9fa985993f6be23ca538a91f89f7
Jack17432 <chrisgraham908@gmail.com>
2024-07-30 16:45:18 +1200

    Changed healthcheck.sh to only check metrics port

--- util/healthcheck.sh
+++ util/healthcheck.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-if [[ -z "${METRICS_PORT}" ]]; then
-  PORT=2222 # Need to make sure this is equal to DEFAULT_APT_PORT in ./src/config.rs
-else
-  # shellcheck disable=SC2034
-  PORT=$METRICS_PORT
-fi
-
-curl -f http://localhost:"$PORT"/health
+curl -f http://localhost:2222/health
