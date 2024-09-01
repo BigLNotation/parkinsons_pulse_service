88d71b8e4aec5df952c911b2c76c8fc0ecbc10d1
clear <alexwillmcleod@gmail.com>
2024-09-02 01:19:09 +1200

    fixed non-public test issue

--- src/app/models/mod.rs
+++ src/app/models/mod.rs
@@ -20,6 +20,9 @@ pub struct User {
 ///
 /// # Examples
 /// ```
+/// use parkinsons_pulse_service::app::models::*;
+/// use mongodb::bson::{oid::ObjectId, DateTime};
+///
 /// Form {
 ///     id: Some(ObjectId::new()),
 ///     title: String::from("Tremors"),
@@ -92,20 +95,20 @@ pub struct User {
 ///             submitted_by: ObjectId::new(),
 ///         }),
 ///     ],
-/// }
+/// };
 /// ```
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Form {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
-    id: Option<ObjectId>,
+    pub id: Option<ObjectId>,
     /// Title of the form for clients
-    title: String,
-    created_by: ObjectId,
-    created_at: DateTime,
+    pub title: String,
+    pub created_by: ObjectId,
+    pub created_at: DateTime,
     /// List of questions in the form
-    questions: Vec<Question>,
+    pub questions: Vec<Question>,
     /// List of events such as a user filling in a form or a moderator updating the form
-    events: Vec<Event>,
+    pub events: Vec<Event>,
 }
 
 /// This represents a form event, either filling in the form and submitting it, or changing a question
@@ -119,22 +122,22 @@ pub enum Event {
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct QuestionEdited {
     /// This is the ID of the question in the form being changed
-    question_id: ObjectId,
-    former_question: Question,
-    new_question: Question,
-    edited_by: ObjectId,
-    edited_at: DateTime,
+    pub question_id: ObjectId,
+    pub former_question: Question,
+    pub new_question: Question,
+    pub edited_by: ObjectId,
+    pub edited_at: DateTime,
 }
 
 /// This is how we represent a form being filled
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct FormSubmitted {
     /// This is a list of all of the questions and the answers that were selected or entered
-    answers: Vec<QuestionAndAnswer>,
+    pub answers: Vec<QuestionAndAnswer>,
     /// This is the ID of the user that submitted the form
-    submitted_by: ObjectId,
+    pub submitted_by: ObjectId,
     /// This is the time that they submitted it
-    submitted_at: DateTime,
+    pub submitted_at: DateTime,
 }
 
 /// This represents a form question for clients to answer
@@ -166,33 +169,33 @@ pub enum QuestionAndAnswer {
 /// Free form question with some validation rules you could apply
 #[derive(Serialize, Deserialize, Clone, Debug, Default)]
 pub struct FreeFormQuestion {
-    _id: ObjectId,
-    title: String,
-    max_length: u64,
-    min_length: u64,
+    pub _id: ObjectId,
+    pub title: String,
+    pub max_length: u64,
+    pub min_length: u64,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, Default)]
 pub struct SliderQuestion {
-    _id: ObjectId,
-    title: String,
-    units: Option<String>,
-    low: f64,
-    high: f64,
-    step: f64,
+    pub _id: ObjectId,
+    pub title: String,
+    pub units: Option<String>,
+    pub low: f64,
+    pub high: f64,
+    pub step: f64,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, Default)]
 pub struct MultichoiceQuestion {
-    _id: ObjectId,
-    title: String,
-    options: Vec<MultichoiceQuestionOption>,
-    min_selected: u64,
-    max_selected: u64,
+    pub _id: ObjectId,
+    pub title: String,
+    pub options: Vec<MultichoiceQuestionOption>,
+    pub min_selected: u64,
+    pub max_selected: u64,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, Default)]
 pub struct MultichoiceQuestionOption {
-    name: String,
-    _id: ObjectId,
+    pub name: String,
+    pub _id: ObjectId,
 }
