0e0f052e77fb7bda0b6506f98c450066f7c3726e
clear <alexwillmcleod@gmail.com>
2024-10-10 15:27:51 +1300

    added tokens and caregiver adding function

---
+++ src/app/caregiver/mod.rs
@@ -0,0 +1,15 @@
+pub mod add;
+pub mod generate;
+
+use axum::{
+    routing::{get, post},
+    Router,
+};
+
+use super::AppState;
+
+pub fn router() -> Router<AppState> {
+    Router::new()
+        .route("/generate", post(generate::generate))
+        .route("/add/:token", post(add::add_caregiver))
+}
